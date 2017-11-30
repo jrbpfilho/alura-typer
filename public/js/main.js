7d9880b07c29b2492ef3064c9ce4d755788b5652
Jose Ruy Borges <jrbpfilho@gmail.com>
2017-11-30 10:50:25 -0200

    Aula 04

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,62 @@
-$(".frase");
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-
-var tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numPalavras);
-
 var campo = $(".campo-digitacao");
-campo.on("input", function() {
-    var conteudo = campo.val();
-    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+var tempoInicial = $("#tempo-digitacao").text();
 
-    $("#contador-palavras").text(qtdPalavras);
+$(function() {
+  atualizaTamanhoFrase();
+  inicializaContadores();
+  inicializaCronometro();
+  $("#botao-reiniciar").click(reiniciaJogo);
 
-    var qtdCaracteres = conteudo.length;
-    $("#contador-caracteres").text(qtdCaracteres);
 });
 
+function atualizaTamanhoFrase() {
+  var frase = $(".frase").text();
+  var numPalavras = frase.split(" ").length;
 
-var tempoRestante = $("#tempo-digitacao").text();
+  var tamanhoFrase = $("#tamanho-frase");
+  tamanhoFrase.text(numPalavras);
+}
 
-campo.one("focus", function() {
 
-    var cronometroID = setInterval(function() {
-        tempoRestante--;
-        $("#tempo-digitacao").text(tempoRestante);
-        if(tempoRestante < 1) {
-            campo.attr("disabled", true);
-            clearInterval(cronometroID);
-        }
-    }, 1000);
-});
+function inicializaContadores() {
+
+  campo.on("input", function() {
+      var conteudo = campo.val();
+      var qtdPalavras = conteudo.split(/\S+/).length - 1;
+
+      $("#contador-palavras").text(qtdPalavras);
+
+      var qtdCaracteres = conteudo.length;
+      $("#contador-caracteres").text(qtdCaracteres);
+  });
+
+}
+
+function inicializaCronometro() {
+  var tempoRestante = $("#tempo-digitacao").text();
+
+  campo.one("focus", function() {
+
+      $("#botao-reiniciar").attr("disabled", true);
+
+      var cronometroID = setInterval(function() {
+          tempoRestante--;
+          $("#tempo-digitacao").text(tempoRestante);
+          if(tempoRestante < 1) {
+              campo.attr("disabled", true);
+              clearInterval(cronometroID);
+              $("#botao-reiniciar").attr("disabled", false);
+          }
+      }, 1000);
+  });
+}
+
+function reiniciaJogo() {
+  campo.attr("disabled", false);
+  //inicializando os campos
+  campo.val("");
+  $("#contador-palavras").text("0");
+  $("#contador-caracteres").text("0");
+  $("#tempo-digitacao").text(tempoInicial);
+  inicializaCronometro();
+}
